afa3c7f5634c305edb014713db5224a7d619bcce
Tero Niemi <talamus@gmail.com>
2022-08-30 00:03:54 +0300

    Helpful comments and translations :)

--- other_tests/suolakurkku.py
+++ other_tests/suolakurkku.py
@@ -1,31 +1,32 @@
+# This programs turns complex objects into a binary blob -- and restores them back to life.
+
 import pickle
-from pprint import pprint
 
-class VaikeaLuokka:
-    nimi = "marsu"
-    hamsteri = False
-    lukumaara = 123
+class VaikeaLuokka:          # ComplexClass
+    nimi = "marsu"           # name = "guinea pig"
+    hamsteri = False         # not a hampster!
+    lukumaara = 123          # count = 123
 
-    def make_hamster(self):
+    def make_hamster(self):  # Force to be a hampster!
         self.hamsteri = True
 
-    def __str__(self):
+    def __str__(self):       # Return this object as a string
         return "Marsu on " + str(self.__dict__)
 
-marsu1 = VaikeaLuokka()
+# Main program:
+
+marsu1 = VaikeaLuokka()      # guinea_pig_1 = ComplexClass()
 marsu2 = VaikeaLuokka()
 
-marsu2.make_hamster()
+marsu2.make_hamster()        # Force guinea_pig_2 to be a hampster! (Heresy!)
 
-lista = [ marsu1, marsu2 ]
+lista = [ marsu1, marsu2 ]   # List = all the guinea pigs
 for marsu in lista:
-    print(marsu)
+    print(marsu)             # Print them!
 
-pikkelsi = pickle.dumps(lista)
-print("pikkelsi:", pikkelsi)
+pikkelsi = pickle.dumps(lista)   # Pickle the guinea pigs! 
+print("pikkelsi:", pikkelsi)     # Print out!
 
-lista_kakkonen = pickle.loads(pikkelsi)
-for marsu in lista_kakkonen:
-    print(marsu)
-
-
+lista_kakkonen = pickle.loads(pikkelsi)   # Turn pickle into a new list
+for marsu in lista_kakkonen:  
+    print(marsu)             # Print them!
